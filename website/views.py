6c8e797633e8af6c417ce97d65f97a7e59bf48e6
elx29H <133196388+elx29H@users.noreply.github.com>
2023-05-19 16:45:55 +0300

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+
+from myPanel.forms import MessagesPostForm
+from myPanel.models import BlogPost, Messages
+from django.shortcuts import render, get_object_or_404
+# Create your views here.
+def displayAll(request) :
+    posts = BlogPost.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def displayPost(request, pk) :
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'postDetails.html', {'post': post})
+
+
+def about(request) :
+    return render(request, 'about.html')
+
+def contact(request) :
+    if request.method == 'POST':
+        form = MessagesPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('contact')
+    else:
+        form = MessagesPostForm()
+    return render(request, 'contact.html',{'form': form})
+
+def team(request) :
+    return render(request, 'team.html')
